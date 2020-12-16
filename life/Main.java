49c1590f25b4323e6c62a8c08276a599e3256e57
wojtekolesinski <64974153+wojtekolesinski@users.noreply.github.com>
2020-12-16 19:06:27 +0100

    animating in console

--- life/Main.java
+++ life/Main.java
@@ -1,27 +1,43 @@
 package life;
 
-import java.util.Random;
+
+import java.lang.Thread;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
+    public static void clear() {
+        try {
+            if (System.getProperty("os.name").contains("Windows"))
+                new ProcessBuilder("cmd","/c","cls").inheritIO().start().waitFor();
+            else
+                Runtime.getRuntime().exec("clear");
+        }
+        catch (IOException | InterruptedException e) {}
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         int n = scanner.nextInt();
-        long s = scanner.nextLong();
-        int m = scanner.nextInt();
+        long s = 10;
+        int m = 9;
 
         Universe life = new Universe(n, s);
+//        life.print();
         for (int k = 0; k < m; k++) {
+            life.print();
             life = Generations.getNextGeneration(life);
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            clear();
         }
 
-        for (int i = 0; i < life.grid.length; i++) {
-            for (int j = 0; j < life.grid[0].length; j++) {
-                System.out.print(life.grid[i][j] ? "O":" ");
-            }
-            System.out.println();
-        }
+
+//        clear();
 
     }
 }
--- life/Universe.java
+++ life/Universe.java
@@ -4,6 +4,7 @@ import java.util.Random;
 
 public class Universe {
     boolean[][] grid;
+    static int generations = 0;
 
     public Universe(int n, long s) {
         this.grid = new boolean[n][n];
@@ -15,9 +16,34 @@ public class Universe {
                 grid[i][j] = random.nextBoolean();
             }
         }
+        generations++;
     }
 
     public Universe (boolean[][] newGrid) {
         this.grid = newGrid;
+        generations++;
+    }
+
+    protected int getAlive() {
+        int counter = 0;
+        for (int i = 0; i < this.grid.length; i++) {
+            for (int j = 0; j < this.grid[i].length; j++) {
+                if (this.grid[i][j]) {
+                    counter++;
+                }
+            }
+        }
+        return counter;
+    }
+
+    public void print() {
+        System.out.println("Generation #" + generations);
+        System.out.println("Alive: " + this.getAlive());
+        for (int i = 0; i < this.grid.length; i++) {
+            for (int j = 0; j < this.grid[0].length; j++) {
+                System.out.print(this.grid[i][j] ? "O":" ");
+            }
+            System.out.println();
+        }
     }
 }
